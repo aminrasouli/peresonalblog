7169c4dbefc57473c0ad0f573357a09c6fdcf434
Amin Rasouli <aminrasoli379@gmail.com>
2019-08-04 20:39:37 +0430

    add loader

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 const withSass = require('@zeit/next-sass')
 const withCSS = require('@zeit/next-css')
 
-module.exports = withSass(withCSS({}))
+module.exports = withSass(withCSS({
+    webpack: (config) => {
+        config.module.rules.push({
+            test: /\.md$/,
+            use: 'raw-loader'
+        });
+        return config
+    }
+}))

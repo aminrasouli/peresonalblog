4f9b9df32d2b723a95f73ae51e8037ea5597bd0c
Amin Rasouli <aminrasoli379@gmail.com>
2019-08-04 20:49:47 +0430

    =

--- pages/theme/Layout.js
+++ pages/theme/Layout.js
@@ -8,9 +8,16 @@ class Layout extends Component {
 	constructor(props) {
 		super(props);
 	}
+	componentDidMount() {
+		document.getElementById('preloder').classList.add('hide-preloder')
+	}
 
 	render() {
 		return <div>
+			{/* TODO:: add preloader */}
+			<div id="preloder" className="animated fadeOut">
+				<div className="loader"/>
+			</div>
 			<Header/>
 				<div className="animated fadeIn faster">
 					{this.props.children}

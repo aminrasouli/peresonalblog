6344846497c82575df75f9f229c9a46a19f6cb6b
Amin Rasouli <aminrasoli379@gmail.com>
2019-09-03 20:58:35 +0430

    fix social link url

--- core/components/SocialLink.js
+++ core/components/SocialLink.js
@@ -9,11 +9,11 @@ class socialLink extends Component {
     render() {
         if (this.props.isShow) {
             return <div className="social-links" style={{textAlign: 'center'}}>
-                <a href=""><span className="fa fa-telegram"/></a>
-                <a href=""><span className="fa fa-instagram"/></a>
-                <a href=""><span className="fa fa-twitter"/></a>
-                <a href=""><span className="fa fa-github"/></a>
-                <a href=""><span className="fa fa-gitlab"/></a>
+                <a href="http://telegram.me/AminDev"><span className="fa fa-telegram"/></a>
+                <a href="http://instagram.com/mmdamin.me"><span className="fa fa-instagram"/></a>
+                <a href="https://twitter.com/rasouli_me"><span className="fa fa-twitter"/></a>
+                <a href="https://github.com/aminrasouli"><span className="fa fa-github"/></a>
+                <a href="https://gitlab.com/aminrasouli"><span className="fa fa-gitlab"/></a>
             </div>;
         }
         return null

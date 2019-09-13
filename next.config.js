3ca0a303e611697aa8c4cc10d2e19617620b80d4
Amin Rasouli <aminrasoli379@gmail.com>
2019-09-13 04:58:06 +0430

    add css minification

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 const withPlugins = require('next-compose-plugins');
 const withSass = require('@zeit/next-sass')
 const withCSS = require('@zeit/next-css')
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
-module.exports = withPlugins([withSass, withCSS], {
-    smartSlashes: true
-});
+const nextConfig = {
+    smartSlashes: true,
+    webpack(config, options) {
+        config.optimization.minimizer = [];
+        config.optimization.minimizer.push(new OptimizeCSSAssetsPlugin({}));
+
+        return config;
+    }
+};
+module.exports = withPlugins([withSass, withCSS], nextConfig);
